9b33f43734a0fa636ed2d42216e709ea1a7847bc
aswin-jacob-thomas <aswinjacobthomas007@gmail.com>
2019-10-31 15:59:13 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -5,134 +5,53 @@ var Cesium = require('cesium/Cesium');
 
 // Example app
 
-var viewer = new Cesium.Viewer('cesiumContainer');
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    terrainProvider: Cesium.createWorldTerrain()
+});
+
+viewer.scene.globe.depthTestAgainstTerrain = true;
 
 var scene = viewer.scene;
 scene.debugShowFramesPerSecond = true;
 
-Cesium.Math.setRandomNumberSeed(315);
 
-var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883));
-var emitterInitialLocation = new Cesium.Cartesian3(0.0, 0.0, 100.0);
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkMzYyNDZmZi1lYTdhLTQwMDgtOGRhZC03ZDE5YTlkYmVkMGMiLCJpZCI6NDAxOSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTUzOTYzODc1OX0.Kb7k51vZGYR5F7btrBIAuSan3ZNyKY_AWrFv1cLFUFk';
 
-var particleCanvas;
+var initialPosition = Cesium.Cartesian3.fromDegrees(-95.381735, 29.749122, 753);
+var initialOrientation = new Cesium.HeadingPitchRoll.fromDegrees(21.27879878293835, -21.34390550872461, 0.0716951918898415);
+viewer.scene.camera.setView({
+    destination: initialPosition,
+    orientation: initialOrientation,
+    endTransform: Cesium.Matrix4.IDENTITY
+});
 
-function getImage() {
-    if (!Cesium.defined(particleCanvas)) {
-        particleCanvas = document.createElement('canvas');
-        particleCanvas.width = 20;
-        particleCanvas.height = 20;
-        var context2D = particleCanvas.getContext('2d');
-        context2D.beginPath();
-        context2D.arc(8, 8, 8, 0, Cesium.Math.TWO_PI, true);
-        context2D.closePath();
-        context2D.fillStyle = 'rgb(255, 255, 255)';
-        context2D.fill();
-    }
-    return particleCanvas;
-}
+var tileset = viewer.scene.primitives.add(
+    new Cesium.Cesium3DTileset({
+        url: Cesium.IonResource.fromAssetId(37161)
+    })
+);
 
-var minimumExplosionSize = 30.0;
-var maximumExplosionSize = 100.0;
-var particlePixelSize = 7.0;
-var burstSize = 400.0;
-var lifetime = 10.0;
-var numberOfFireworks = 20.0;
+var tileset1 = viewer.scene.primitives.add(
+    new Cesium.Cesium3DTileset({
+        url: Cesium.IonResource.fromAssetId(40446)
+    })
+);
 
-var emitterModelMatrixScratch = new Cesium.Matrix4();
-
-function createFirework(offset, color, bursts) {
-    var position = Cesium.Cartesian3.add(emitterInitialLocation, offset, new Cesium.Cartesian3());
-    var emitterModelMatrix = Cesium.Matrix4.fromTranslation(position, emitterModelMatrixScratch);
-    var particleToWorld = Cesium.Matrix4.multiply(modelMatrix, emitterModelMatrix, new Cesium.Matrix4());
-    var worldToParticle = Cesium.Matrix4.inverseTransformation(particleToWorld, particleToWorld);
-
-    var size = Cesium.Math.randomBetween(minimumExplosionSize, maximumExplosionSize);
-    var particlePositionScratch = new Cesium.Cartesian3();
-    var force = function(particle) {
-        var position = Cesium.Matrix4.multiplyByPoint(worldToParticle, particle.position, particlePositionScratch);
-        if (Cesium.Cartesian3.magnitudeSquared(position) >= size * size) {
-            Cesium.Cartesian3.clone(Cesium.Cartesian3.ZERO, particle.velocity);
+function colorByDistance() {
+    tileset.style = new Cesium.Cesium3DTileStyle({
+        defines : {
+            distance : 'distance(vec2(${Longitude}, ${Latitude}), vec2(-1.6647633256709604,0.519209520751604))'
+        },
+        color : {
+           conditions : [
+                ['${distance} > 0.0012',"color('gray')"],
+                ['${distance} > 0.0008', "mix(color('yellow'), color('red'), (${distance} - 0.008) / 0.0004)"],
+                ['${distance} > 0.0004', "mix(color('green'), color('yellow'), (${distance} - 0.0004) / 0.0004)"],
+                ['${distance} < 0.00001', "color('white')"],
+                ['true', "mix(color('blue'), color('green'), ${distance} / 0.0004)"]
+            ]
         }
-    };
-
-    var normalSize = (size - minimumExplosionSize) / (maximumExplosionSize - minimumExplosionSize);
-    var minLife = 0.3;
-    var maxLife = 1.0;
-    var life = normalSize * (maxLife - minLife) + minLife;
-
-    scene.primitives.add(new Cesium.ParticleSystem({
-        image : getImage(),
-        startColor : color,
-        endColor : color.withAlpha(0.0),
-        life : life,
-        speed : 100.0,
-        width : particlePixelSize,
-        height : particlePixelSize,
-        rate : 0,
-        emitter : new Cesium.SphereEmitter(0.1),
-        bursts : bursts,
-        lifeTime : lifetime,
-        forces : [force],
-        modelMatrix : modelMatrix,
-        emitterModelMatrix : emitterModelMatrix
-    }));
+    });
 }
 
-var xMin = -100.0;
-var xMax = 100.0;
-var yMin = -80.0;
-var yMax = 100.0;
-var zMin = -50.0;
-var zMax = 50.0;
-
-var colorOptions = [{
-    minimumRed : 0.75,
-    green : 0.0,
-    minimumBlue : 0.8,
-    alpha : 1.0
-}, {
-    red : 0.0,
-    minimumGreen : 0.75,
-    minimumBlue : 0.8,
-    alpha : 1.0
-}, {
-    red : 0.0,
-    green : 0.0,
-    minimumBlue : 0.8,
-    alpha : 1.0
-}, {
-    minimumRed : 0.75,
-    minimumGreen : 0.75,
-    blue : 0.0,
-    alpha : 1.0
-}];
-
-for (var i = 0; i < numberOfFireworks; ++i) {
-    var x = Cesium.Math.randomBetween(xMin, xMax);
-    var y = Cesium.Math.randomBetween(yMin, yMax);
-    var z = Cesium.Math.randomBetween(zMin, zMax);
-    var offset = new Cesium.Cartesian3(x, y, z);
-    var color = Cesium.Color.fromRandom(colorOptions[i % colorOptions.length]);
-
-    var bursts = [];
-    for (var j = 0; j < 3; ++j) {
-        bursts.push(new Cesium.ParticleBurst({
-            time : Cesium.Math.nextRandomNumber() * lifetime,
-            minimum : burstSize,
-            maximum : burstSize
-        }));
-    }
-
-    createFirework(offset, color, bursts);
-}
-
-var camera = viewer.scene.camera;
-var cameraOffset = new Cesium.Cartesian3(-300.0, 0.0, 0.0);
-camera.lookAtTransform(modelMatrix, cameraOffset);
-camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
-
-var toFireworks = Cesium.Cartesian3.subtract(emitterInitialLocation, cameraOffset, new Cesium.Cartesian3());
-Cesium.Cartesian3.normalize(toFireworks, toFireworks);
-var angle = Cesium.Math.PI_OVER_TWO - Math.acos(Cesium.Cartesian3.dot(toFireworks, Cesium.Cartesian3.UNIT_Z));
-camera.lookUp(angle);
+colorByDistance();
--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,7 @@ module.exports = [{
         new CopyWebpackPlugin([{from: path.join(cesiumSource, cesiumWorkers), to: 'Workers'}]),
         new CopyWebpackPlugin([{from: path.join(cesiumSource, 'Assets'), to: 'Assets'}]),
         new CopyWebpackPlugin([{from: path.join(cesiumSource, 'Widgets'), to: 'Widgets'}]),
+        new CopyWebpackPlugin([{from: path.join(cesiumSource, 'ThirdParty'), to: 'ThirdParty'}]),
         new webpack.DefinePlugin({
             // Define relative base path in cesium for loading assets
             CESIUM_BASE_URL: JSON.stringify('')
